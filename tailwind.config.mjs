4d672edc8647ac28c2940601141498dc43fa5785
Aman Mollah <work.amanmollah@gmail.com>
2023-11-02 20:07:30 +0530

    homepage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,6 @@ export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./public/**/*.astro",
-    "./src/**/*.{astro,js,jsx,ts,tsx,vue}",
     "node_modules/preline/dist/*.js",
   ],
   theme: {
